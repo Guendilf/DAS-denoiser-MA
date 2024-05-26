fdf86e912096a30eca488acafdc6acee85d4dba2
guendilf <alexander.dwucet@tu-dortmund.de>
2024-05-26 18:54:18 +0200

    fix n2score

--- main.py
+++ main.py
@@ -105,17 +105,18 @@ def loss_n2void(original_images, noise_images, model, device, num_patches_per_im
     return loss_function(denoised_pixel, target_pixel), denoised, patches, clean_patches
 
 def loss_n2same(noise_images, device, model, lambda_inv=2):
-    mask, maked_points = (Mask.cut2self_mask((noise_images.shape[2],noise_images.shape[3]), noise_images.shape[0], mask_size=(1, 1), mask_percentage=0.0005)) #0,5% Piel maskieren
+    mask, marked_points = (Mask.cut2self_mask((noise_images.shape[2],noise_images.shape[3]), noise_images.shape[0], mask_size=(1, 1), mask_percentage=0.0005)) #0,5% Piel maskieren
     mask = mask.to(device)
     mask = mask.unsqueeze(1)  # (b, 1, w, h)
     mask = mask.expand(-1, 3, -1, -1) # (b, 3, w, h)
-    masked_input = (1-mask) * noise_images
+    masked_input = (1-mask) * noise_images #delete masked pixels in noise_img
     denoised = model(noise_images)
     denoised_mask = model(masked_input)
     mse = torch.nn.MSELoss()
-    loss_rec = mse(denoised, noise_images) #TODO:,dim=1,2,3
-    loss_inv = mse(denoised*mask, denoised_mask*mask)
-    return loss_rec/(noise_images.shape[1]*noise_images.shape[2]*noise_images.shape[3])+ lambda_inv * (loss_inv/maked_points).sqrt(), denoised, denoised_mask #J = count of maked_points
+    loss_rec = torch.mean((denoised-noise_images)**2) # mse(denoised, noise_images)
+    loss_inv = torch.mean((denoised-denoised_mask)**2)# mse(denoised, denoised_mask)
+    loss = loss_rec/(noise_images.shape[1]*noise_images.shape[2]*noise_images.shape[3])+ lambda_inv * (loss_inv/marked_points).sqrt()
+    return loss, denoised, denoised_mask #J = count of maked_points
 
 
 def n2n_loss_for_das(denoised, target):
@@ -159,6 +160,8 @@ def train(model, optimizer, device, dataLoader, methode, sigma, mode, store, epo
             loss, denoised, noise_images = loss_n2score(noise_images, sigma_min=1, sigma_max=30, q=batch_idx/len(dataLoader), 
                                                         device=device, model=model, methode=methode)
             #TODO: abhängig vom Noise, wird rekonstruiertes Endbild noch bearbeitet -> Suplemenrtary
+            if "tweedie" in methode:
+                denoised = denoised + 25**2*loss
 
         elif methode == "n2self":
             loss, denoised, masked_noise_image = loss_n2self(noise_images, batch_idx, model)
@@ -198,6 +201,9 @@ def train(model, optimizer, device, dataLoader, methode, sigma, mode, store, epo
                 torch.save(model.state_dict(), model_save_path)
                 print(f"saved new best model at path {model_save_path}")
                 #denoised_norm = normalize_image(denoised)
+                if methode == "n2void":
+                    skip = denoised.shape[0] / 64 #64=ursprüngllichhe Batchgröße
+                    denoised = denoised[::skip] # zeige nur jedes 6. Bild an (im path wird aus einem bild 6 wenn die Batchhgröße = 64)
                 grid = make_grid(denoised, nrow=16, normalize=False) # Batch/number bilder im Raster
                 writer.add_image('Denoised Images', grid, global_step=epoch * len(dataLoader) + batch_idx)
                 #show_tensor_as_picture(denoised)
@@ -212,7 +218,7 @@ def train(model, optimizer, device, dataLoader, methode, sigma, mode, store, epo
 def main(argv):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     #device = "cuda:3"
-    methoden_liste = ["n2n_orig", "score", "n2self", "n2void", "n2same"]
+    methoden_liste = ["n2n_orig", "score_ar", "score_ar tweedie", "n2self", "n2void", "n2same"]
     methode = methoden_liste[3]
     
     #run = wandb.init(entity="", project="my-project-name", anonymous="allow")
